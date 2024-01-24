e4f07c78a5cd4d81d5a3f1af5f345dc640e6872a
James <jamesjglee73@gmail.com>
2024-01-23 23:26:46 -0500

    Bi-directional

--- server/main.go
+++ server/main.go
@@ -1,1 +1,27 @@
 package main
+
+import(
+	"log"
+	"net"
+	pb "github.com/GodlyV/grpc-demo/proto"
+	"google.golang.org/grpc"
+)
+
+const(
+	port = ":8080"
+)
+type helloServer struct{
+	pb.GreetServiceServer
+}
+func main(){
+	lis,err:= net.Listen("tcp",port)
+	if err != nil{
+		log.Fatalf("Failed to start the server %v",err)
+	}
+	grpcServer := grpc.NewServer()
+	log.Printf("server started at %v", lis.Addr())
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	if err := grpcServer.Serve(lis); err !=nil{
+		log.Fatalf("Failed to start: %v",err)
+	}
+}
